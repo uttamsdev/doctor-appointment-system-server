12b171164e78ca356355b530e2492d6f55fcca2b
uttamsaha <uttamsaha982@gmail.com>
2022-05-20 11:12:53 +0600

    delete doctor api added

--- index.js
+++ index.js
@@ -158,12 +158,24 @@ async function run() {
       res.send({ success: true, result });
     });
 
+    //get doctors form DB
+    app.get('/doctor', verifyJWT, verifyAdmin, async(req, res) => {
+      const doctors = await doctorCollection.find().toArray();
+      res.send(doctors);
+    });
     //adding doctor details in databse
     app.post('/doctor', verifyJWT, verifyAdmin, async(req,res) => {
       const doctor = req.body;
       const result = await doctorCollection.insertOne(doctor);
       res.send(result);
-    })
+    });
+
+    app.delete('/doctor/:email', verifyJWT, verifyAdmin, async(req,res) => {
+      const email = req.params.email;
+      const filter = {email: email};
+      const result = await doctorCollection.deleteOne(filter);
+      res.send(result);
+    });
   } finally {
     // client.close();
   }
